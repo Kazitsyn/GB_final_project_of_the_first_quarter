3a3a8f61b92dbfac4e38b0dca7aea5fe2f92e4bb
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2023-01-25 22:21:47 +0300

    rough decision

---
+++ REEDME.md
@@ -0,0 +1,4 @@
+
+<image src = "https://gbcdn.mrgcdn.ru/uploads/asset/4283449/attachment/1251e74b703108ee483caaa98787097d.png">
+
+# Итоговая проверочная работа
--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] primer1 = {"hello", "2", ":-)"};
+string [] primer2 = {"1234", "1567", "computer science"};
+string [] primer3 = {"Russia", "Demmark", "Kazan"};
+
+
+int count = 0;
+foreach (string item in primer1)
+{
+    if (item.Length <= 3)
+    {
+        count++;
+    } 
+    
+} 
+
+string[] result = new string[count];
+int index = 0;
+
+for (int i = 0; i < primer1.Length; i++)
+{
+    if (primer1[i].Length <= 3) 
+    {
+        result[index] = primer1[i];
+        index++;
+    }
+}
+
+for (int i = 0; i < result.Length; i++)
+{
+    if (i == 0)
+    {
+        Console.Write($"[{result[i]}, ");
+    }
+    else if (i< result.Length-1)
+    {
+        Console.Write($"{result[i]}, ");
+    }
+    else 
+    {
+        Console.Write($"{result[i]}]");
+    }
+}
