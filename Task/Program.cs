987c7ca0de4d3f19d8eb085b552640c43519e34b
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2023-01-26 20:27:54 +0300

    add methods

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,42 +1,86 @@
-﻿string [] primer1 = {"hello", "2", ":-)"};
-string [] primer2 = {"1234", "1567", "computer science"};
+﻿string[] InputArr()
+{
+    Console.Write("Введите строки через пробел: ");
+    string words = Console.ReadLine()?? " ";
+    return words.Split(' ');
+}
+
+
+string[] SelectItem(string[] arr, int selectLength = 3)
+{
+    int count = 0;
+
+    foreach (string item in arr)
+    {
+        if (item.Length <= selectLength)
+        {
+            count++;
+        } 
+    }
+    string[] result = new string[count];
+
+    if (count == 0) return result;
+    else
+    {
+        int index = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i].Length <= selectLength) 
+            {
+                result[index] = arr[i];
+                index++;
+            }
+        }
+        return result;
+    }
+}
+
+void Print(string[] arr)
+{
+    if (arr.Length == 0) Console.WriteLine("[]");
+    else
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (i == 0)
+            {
+                Console.Write($"[{arr[i]}");
+                if (i == arr.Length-1) Console.Write("]");
+                else Console.Write(", ");
+            }
+            else if (i< arr.Length-1)
+            {
+                Console.Write($"{arr[i]}, ");
+            }
+            else 
+            {
+                Console.Write($"{arr[i]}]");
+            }
+        }
+        
+    }
+}
+
+void PrintResult(string[] arr, string[] result)
+{
+    Print(arr);
+    Console.Write(" --> ");
+    Print(result);
+    Console.WriteLine();
+}
+
+string [] primer1 = {"hello", "2", ":-)"};
+string [] primer2 = {"1234", "1567", "-2", "computer science"};
 string [] primer3 = {"Russia", "Demmark", "Kazan"};
 
+PrintResult(primer1, SelectItem(primer1));
+PrintResult(primer2, SelectItem(primer2));
+PrintResult(primer3, SelectItem(primer3));
 
-int count = 0;
-foreach (string item in primer1)
-{
-    if (item.Length <= 3)
-    {
-        count++;
-    } 
-    
-} 
+string[] primerInput = InputArr();
 
-string[] result = new string[count];
-int index = 0;
+PrintResult(primerInput, SelectItem(primerInput));
 
-for (int i = 0; i < primer1.Length; i++)
-{
-    if (primer1[i].Length <= 3) 
-    {
-        result[index] = primer1[i];
-        index++;
-    }
-}
 
-for (int i = 0; i < result.Length; i++)
-{
-    if (i == 0)
-    {
-        Console.Write($"[{result[i]}, ");
-    }
-    else if (i< result.Length-1)
-    {
-        Console.Write($"{result[i]}, ");
-    }
-    else 
-    {
-        Console.Write($"{result[i]}]");
-    }
-}
+
+
